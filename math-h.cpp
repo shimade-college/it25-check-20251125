cc709cdd786c49792622eaf247ecf4e4084d6d20
shigorefu <eduard@shimade.ac.jp>
2025-11-21 17:26:48 +0900

    課題

---
+++ math-h.cpp
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <math.h>
+
+// 問題1
+void task1() {
+    // TODO: 角度30度の cos を計算して表示しなさい。
+}
+
+// 問題2
+void task2() {
+    // TODO: 225 の平方根を求めて表示しなさい。
+}
+
+// 問題3
+void task3() {
+    // TODO: -123.45 の絶対値を求めて表示しなさい。
+}
+
+// 問題4
+void task4() {
+    // TODO: 2 の 10 乗を計算して表示しなさい。
+}
+
+// 問題5
+void task5() {
+    // TODO: e の 1乗, 2乗, 3乗を表示しなさい。
+}
+
+// 問題6
+void task6() {
+    // TODO: 100 の自然対数 log(100) を求めなさい。
+}
+
+// 問題7
+void task7() {
+    // TODO: 角度60度の sin, cos, tan をまとめて表示しなさい。
+}
+
+
+// 問題8
+void task8() {
+    // (x1,y1) = (3,4), (x2,y2) = (-1,7)
+    // TODO: ユークリッド距離 √((x2-x1)^2 + (y2-y1)^2) を求めなさい。
+}
+
+// 問題9
+void task9() {
+    // 初項 a=2, 公比 r=3, n=5 のとき
+    // TODO: a * r^(n-1) を計算しなさい。
+}
+
+// 問題10
+void task10 () {
+    // ax^2 + bx + c = 0
+    // 判別式 D = b*b - 4ac
+    // x = (-b ± sqrt(D)) / (2a)
+    // TODO: a=1, b=-3, c=-4 の実数解を求めなさい。
+}
+
+int main() {
+
+                task1();
+        task2();        task3();
+        task4();task5();task6();
+        task6();        task7();
+                task8();
+        task10();       task9();
+
+    return 0;
+}
---
+++ string-h.cpp
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <string.h>
+
+// 問題1
+void task1() {
+    //TODO: "CAT" を dst にコピーして表示しなさい。
+    char src[] = "CAT";
+    char dst[10];
+}
+
+// 問題2
+void task2() {
+    // TODO: "Hello" の長さを表示しなさい。
+    char s[] = "Hello";
+}
+
+// 問題3
+void task3() {
+    // TODO: "ABC" と "ABD" を比較し、結果を表示しなさい。
+    char a[] = "ABC";
+    char b[] = "ABC";
+}
+
+// 問題4
+void task4() {
+    //TODO:  "Hello" と "World" を連結して表示しなさい。
+    char a[20] = "Hello";
+    char b[] = "World";
+}
+
+// 問題5
+void task5() {
+    // TODO "ABCDEFG" と "ABCXXXX" の先頭3文字を比較しなさい。
+    char a[] = "ABCDEFG";
+    char b[] = "ABCXXXX";
+}
+
+// 問題6
+void task6() {
+    //TODO: "Today is sunny day" の中から "sunny" を探しなさい。
+    char s[] = "Today is sunny day";
+}
+
+// 問題7
+void task7() {
+    //TODO: "AABACDAAAABBBAAC" の中で最も多い文字を求めなさい。
+    char s[] = "AABACDAAAABBBAAC";
+}
+
+
+int main() {
+
+    task1();
+            task2();
+                    task3();
+                            task4();
+                    task5();
+            task6();
+    task7();
+
+    return 0;
+}
